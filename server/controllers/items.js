a3c63fa3b7e6b2feed2ab2ceeb8d2d9b3d3e4ecf
kalibani <kalibani.ka@gmail.com>
2017-10-24 21:15:01 +0700

    crud server

---
+++ server/controllers/items.js
@@ -0,0 +1,41 @@
+const models = require('../models/items');
+
+class Items {
+  static findAll(req, res){
+    models.getItems((err, items)=>{
+      if(err){
+        res.send(err)
+      }
+      res.json(items)
+    })
+  }
+
+  static create(req, res){
+    models.addItem(req.body, (err, item)=>{
+      if (err) {
+        res.send(err)
+      }
+      res.json(item)
+    })
+  }
+
+  static update(req, res){
+    models.updateItem(req.params, req.body, (err, item)=>{
+      if (err) {
+        res.send(err)
+      }
+      res.json('Success Update')
+    })
+  }
+
+  static delete(req, res){
+    models.deleteItem(req.params, (err, item)=>{
+      if (err) {
+        res.send(err)
+      }
+      res.send('Success Delete From Items')
+    })
+  }
+
+}
+module.exports = Items;
